3b808bf2ee19a2e4e325eb0222bf105cf415a4d1
LLTEST <chilavert@tut.by>
2017-09-05 00:33:39 +0300

    Update SortByClr.java
    What did you say!?

--- src/ttask1/SortByClr.java
+++ src/ttask1/SortByClr.java
@@ -10,9 +10,7 @@ public class SortByClr implements Comparator<Salad> {
 	 * Method compare Salads by Calories
 	 */
 	public int compare(Salad o1, Salad o2) {
-		System.out.println("asfsag");
-		System.out.println("asfsag");
-		System.out.println("asfsag2353252323523");
+		
 		return o1.foodCalor().compareTo(o2.foodCalor());
 
 	}
